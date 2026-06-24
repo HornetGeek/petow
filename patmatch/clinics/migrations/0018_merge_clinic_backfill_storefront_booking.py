26d9bd1d0155d926fdbd8609a15560b92b469f63
hornetgeek <mmoataz03@gmail.com>
2026-06-24 16:08:32 +0300

    Merge clinic staging migrations

---
+++ patmatch/clinics/migrations/0018_merge_clinic_backfill_storefront_booking.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('clinics', '0015_backfill_clinic_location_points'),
+        ('clinics', '0017_storefrontbooking_customer_user'),
+    ]
+
+    operations = [
+    ]
