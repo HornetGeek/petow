78a3b3defea0f84bdd4f76f1041976ae442b6e92
hornetgeek <mmoataz03@gmail.com>
2026-06-24 16:37:49 +0300

    new changes

--- patmatch/pets/migrations/0032_petlike_storyreaction_engagementevent.py
+++ patmatch/pets/migrations/0032_petlike_storyreaction_engagementevent.py
@@ -7,7 +7,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('pets', '0031_story_storyview_storyreport'),
+        ('pets', '0031_story_storyreport_storyview_and_more'),
     ]
 
     operations = [
